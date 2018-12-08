05503f732a48fff7184c8039ebc8d9cdbb5dbdd8
ponyspy <desade4me@gmail.com>
2018-12-08 18:58:58 +0300

    clean

--- apps/rus_speech/routes/main/_main.js
+++ apps/rus_speech/routes/main/_main.js
@@ -4,10 +4,6 @@ var Model = require(__glob_root + '/models/main.js');
 
 var main = {
 	index: require('./index.js')(Model),
-	projects: require('./works.js')(Model, 'project'),
-	researches: require('./works.js')(Model, 'research'),
-	office: require('./office.js')(Model),
-	cv: require('./cv.js')(Model),
 	options: require('./options.js')(Model)
 };
 
@@ -16,29 +12,6 @@ module.exports = (function() {
 
 	router.route('/')
 		.get(main.index.index);
-
-	router.route('/cv')
-		.get(main.cv.index);
-
-	router.route('/cv/:short_id')
-		.get(main.cv.people);
-
-	router.route('/office')
-		.get(main.office.index);
-
-	router.route('/projects')
-		.get(main.projects.index)
-		.post(main.projects.get_works);
-
-	router.route('/projects/:short_id')
-		.get(main.projects.work);
-
-	router.route('/research')
-		.get(main.researches.index)
-		.post(main.researches.get_works);
-
-	router.route('/research/:short_id')
-		.get(main.researches.work);
 
 	router.route('/lang/:locale').get(function(req, res) {
 		res.cookie('locale', req.params.locale);
